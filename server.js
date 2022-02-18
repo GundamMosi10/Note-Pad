3f8b2f82b3b0f9d5982e69ee6388e6c9531f7632
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-18 15:41:15 -0500

    created a folder for routes and 2 files one for index.js and the other is notes.js, added some code in the index.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // GET route for homepage index.html
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
