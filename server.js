44e6dbb3bb0c6545a1d4f738e3ddc85e383970f9
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-18 15:26:02 -0500

    edited a small typo with the app listening function so it recognizes the PORT

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require('express'); //these are dependencies
+const fs = require('fs');
+const path = require('path');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// GET route for homepage index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+//GET route for notes.html page 
+app.get('/notes', (req, res) => {
+    res.sindFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.listen(PORT, () => 
+    console.log(`app listening at http://localhost:${PORT}`)
+);
