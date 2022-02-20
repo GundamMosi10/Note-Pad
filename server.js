2779487439e9fde604ae52d1caa07872e1226eab
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-19 20:45:21 -0500

    added the post method function to create new notes and join them to the db.json file and be able to read them by stringify them

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); //these are dependencies
 const fs = require('fs');
 const path = require('path');
 const {readFromFile, writeToFile, readAndAppend} = require('./fsUtils');
-const uuid = require('uuid');
+const {v4 : uuidv4} = require('uuid');
 
 
 //server set up
@@ -15,9 +15,17 @@ app.use(express.static('public')); //allows us to use the css and js in the publ
 
 //
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json')); //this should save the notes and joins it to the json database db.json
+    res.sendFile(path.join(__dirname, '/db/db.json')); //this should get the notes that are in the json db
 })
 
+app.post('/api/notes', (req, res) => { //allows us to create new notes 
+    const notes = JSON.parse(fs.readFromFile('/db/db.json')); //parsing this json file turns the information into an object for us to read the file
+    const newNotes = req.body; 
+    newNotes.id = uuidv4(); //gives each new note a unique id so that we can specifically target it when we want
+    notes.push(newNotes); 
+    fs.writeToFile('./db/db.json', JSON.stringify(notes)) //takes the json object and turns it into a string so we can read it
+    res.json(notes);
+});
 
 
 // // This is to read from the db.json file
