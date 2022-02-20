8788217dc4b8d4758d3373d67df18019ce8796a2
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-19 20:17:07 -0500

    changed the way the code gets the notes and puts them in the db.json

--- server.js
+++ server.js
@@ -13,6 +13,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); //allows us to use the css and js in the public folder for the front end
 
+//
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json')); //this should save the notes and joins it to the json database db.json
+})
+
+
+
+// // This is to read from the db.json file
+// app.get('/api/notes', (req, res) => {
+//     readFromFile('./db/db.json').then(function(data){
+//         const notes = JSON.parse(data);
+//             console.log(notes);
+//             res.json(notes)
+//     })
+// })
+
+// //This allows us to create a new note and posts it to the db.json file
+// app.post('/api/notes', (req, res) => {
+//     const note = {
+//         title:req.body.title,
+//         text:req.body.text
+//     }
+//     readAndAppend(note,'./db/db.json')
+// })
+
+
 // GET route for homepage index.html
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
@@ -22,25 +48,6 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
-
-
-// This is to read from the db.json file
-app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then(function(data){
-        const notes = JSON.parse(data);
-            console.log(notes);
-            res.json(notes)
-    })
-})
-//This allows us to create a new note and posts it to the db.json file
-app.post('/api/notes', (req, res) => {
-    const note = {
-        title:req.body.title,
-        text:req.body.text
-    }
-    readAndAppend(note,'./db/db.json')
-})
-
 
 
 app.listen(PORT, () => 
